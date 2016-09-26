ce0186a7b266389bca1dbcd9af2ebc44b92c7370
lee.sharlia <lia@shopstudio-oro.com>
2016-09-26 15:56:41 +0000

    Updated Gem files

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,34 @@
 class MessagesController < ApplicationController
     def index
+        @messages = Message.all
+    end
+    
+    def new
+        @message = Message.new
+    end
+  
+    def create
+  	    @message = Message.new(message_params)
+        if @message.save
+            redirect_to '/messages'
+        else
+            render 'new'
+        end
+    end
+    
+    def update
     end
 
+    def destroy
+        if @message.destroy
+            redirect_to messages_path, notice: "Oops! Say something wrong? Confirm you'd like to delete this message"
+        else 
+            render 'new', notice: "Oh no! I was unable to delete your post"
+        end 
+    end
+  
+    private
+        def message_params
+  	        params.require(:message).permit(:content)
+        end
 end
